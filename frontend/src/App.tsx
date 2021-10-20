7e2f3b775f9283371a24457fa66a8d75110ddee1
ingridsj <ingridsjalmeida@gmail.com>
2021-10-20 18:36:12 -0300

    add: nlw day 02

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { AuthContext } from "./contexts/auth";
+import { LoginBox } from "./components/LoginBox";
+import { MessageList } from "./components/MessageList";
+import { SendMessageForm } from "./components/SendMessageForm";
+
+import styles from "./App.module.scss";
+
+export function App() {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <main
+      className={`${styles.contentWrapper} ${
+        !!user ? styles.contentSigned : ""
+      }`}
+    >
+      <MessageList />
+      {!!user ? <SendMessageForm /> : <LoginBox />}
+    </main>
+  );
+}
